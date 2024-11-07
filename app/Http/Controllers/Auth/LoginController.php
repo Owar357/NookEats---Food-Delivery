f9ae0834da70cf8c49107a4b7784d693333d23c0
Anderson <lm22001@itcha.edu.sv>
2024-11-07 16:42:44 -0600

    Se agrego la proteccion de rutas y redireccionamiento

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,13 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
     }
+    
+    public function authenticated($request, $user){
+        if($user->rol == 'R'){
+            return redirect()->route('admin-rest');    
+        }elseif ($user->rol == 'A') {
+            return redirect()->route('admin');
+        }
+        return redirect()->route('home');
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\CheckAdmin;
+use App\Http\Middleware\CheckAdminRestaurante;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'auth.admin' => CheckAdmin::class,
+            'auth.rest' => CheckAdminRestaurante::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
